14212105170dcdbaa724ac75a6c90ca6024a37de
Nick <SuperDubPro@gmail.com>
2021-02-13 20:37:46 +0300

    second lesson

---
+++ src/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Button(props) {
+	return <button
+		className={`button ${props.outline ? 'button--outline' : ''}`}
+	>
+		{props.children}
+	</button>
+}
+
+export default Button
